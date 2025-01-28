b8e12df642bb3818a728363c6b1c7869946e0bbd
Socrates Muquepe <socycrates@hotmail.com>
2025-01-28 23:13:23 +0100

    :sparkles: feat: Project Structure, NavBar and Routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from "./shared/shared.module";
+import { HomeModule } from './modules/home/home.module';
+import { MaintenanceModule } from './modules/maintenance/maintenance.module';
+import { ScheduleModule } from './modules/schedule/schedule.module';
 
 @NgModule({
   declarations: [
@@ -12,8 +16,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
-  ],
+    NgbModule,
+    SharedModule,
+    HomeModule,
+    MaintenanceModule,
+    ScheduleModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
